6e185be0b699ee77c3238fd8b28bf701a7747891
Andrii <rod.andriiw@gmail.com>
2024-01-14 17:19:21 +0000

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import imageLogo from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-function generateRandom(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-function Header() {
-  const dynamicWord =
-    reactDescriptions[generateRandom(reactDescriptions.length - 1)];
-  return (
-    <header>
-      <img src={imageLogo} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {dynamicWord} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 function App() {
   return (
     <div>
@@ -37,26 +10,10 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import imageLogo from '../assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function generateRandom(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+export default function Header() {
+  const dynamicWord =
+    reactDescriptions[generateRandom(reactDescriptions.length - 1)];
+  return (
+    <header>
+      <img src={imageLogo} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {dynamicWord} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
