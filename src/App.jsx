0d0a269628e06a1ac6b36d1b2501f559608329a1
Andrii <rod.andriiw@gmail.com>
2024-01-14 14:34:48 +0000

    add dynamicword

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import imageLogo from './assets/react-core-concepts.png'
+import imageLogo from './assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function generateRandom(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function Header() {
+  const dynamicWord =
+    reactDescriptions[generateRandom(reactDescriptions.length - 1)];
+  return (
+    <header>
+      <img src={imageLogo} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {dynamicWord} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src={imageLogo} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
